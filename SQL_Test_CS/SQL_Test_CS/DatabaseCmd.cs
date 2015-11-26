65caea2f5dee0acc04f154975349c44b8a556e12
Ziyun Zhao <zhaoziy@hotmail.com>
2015-11-26 17:15:24 +0800

    new

---
+++ SQL_Test_CS/SQL_Test_CS/DatabaseCmd.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SQL_Test_CS
+{
+    class DatabaseCmd
+    {
+		static string connstr = "Data Source=.;Integrated Security=True;Database=test";
+		public bool SqlExecuteNonQuery(string sqlcmd)
+		{
+			SqlConnection conn = new SqlConnection(connstr);
+			conn.Open();
+			SqlCommand cmd = new SqlCommand(sqlcmd, conn);
+			//SqlTransaction myTran=new SqlTransaction();  
+			//注意，SqlTransaction类无公开的构造函数  
+			SqlTransaction myTran = conn.BeginTransaction();
+			cmd.Connection = conn;
+			cmd.Transaction = myTran;
+			try
+			{
+				//从此开始，基于该连接的数据操作都被认为是事务的一部分
+				//下面绑定连接和事务对象  
+				cmd.CommandText = "USE test";
+				cmd.ExecuteNonQuery();     //更新数据  
+				cmd.CommandText = sqlcmd;
+				cmd.ExecuteNonQuery();
+				//提交事务  
+				myTran.Commit();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				try
+				{
+					myTran.Rollback();
+				}
+				catch (Exception ex2)
+				{
+					MessageBox.Show(ex2.Message);
+				}
+				return false;
+			}
+			finally
+			{
+				myTran.Dispose();
+				cmd.Transaction = null;
+				conn.Close();
+				conn.Dispose();
+			}
+		}
+
+		public bool SqlExecuteReader(string sqlcmd,out SqlDataReader myreader)
+		{
+			SqlConnection conn = new SqlConnection(connstr);
+			conn.Open();
+			SqlCommand cmd = new SqlCommand(sqlcmd, conn);
+			try
+			{
+				cmd.CommandText = "USE test";
+				cmd.ExecuteNonQuery();     //更新数据  
+				cmd.CommandText = sqlcmd;
+				myreader = cmd.ExecuteReader();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				myreader = null;
+				MessageBox.Show(ex.Message);
+				return false;
+			}
+		}
+	}
+}
---
+++ SQL_Test_CS/SQL_Test_CS/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_Test_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			DatabaseCmd select = new DatabaseCmd();
+			string str = "select * from Vehicle";
+			SqlDataReader myreader = null;
+			select.SqlExecuteReader(str, out myreader);
+			if (myreader.Read())
+			{
+				string a = "";
+				a = myreader.GetString(2);
+				MessageBox.Show(a);
+			}
+        }
+	}
+}
